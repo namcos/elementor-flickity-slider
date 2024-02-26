95b9f50cc600a84ebcea1170a626d9a3f73e0af4
James Dunn <quake53@hotmail.com>
2024-02-26 18:13:50 +0000

    Updating plugin name for WP repo upload

--- elementor-flickity-slider.php
+++ elementor-flickity-slider.php
@@ -6,13 +6,17 @@
 	* @author James Dunn
 	*
 	* @wordpress-plugin
-	* Plugin Name:	Flickity Slider for Elementor
-	* Plugin URI:	https://jamesdunnfreelance.co.uk/elementor-flickity-slider-widget/
-	* Description:	Slider widget for Elementor that uses Flickity
-	* Version:		1.2.1
-	* Author:		James Dunn
-	* Author URI:	https://jamesdunnfreelance.co.uk
-	* Text Domain:	flickity-slider-elementor
+	* Plugin Name: Flickity Slider for Elementor
+	* Plugin URI: https://jamesdunnfreelance.co.uk/elementor-flickity-slider-widget/
+	* Description: Slider widget for Elementor that uses Flickity.
+	* Version: 1.2.1
+	* Requires at least: 5.2
+	* Requires PHP: 7.3.9
+	* Author: James Dunn
+	* Author URI: https://jamesdunnfreelance.co.uk
+	* Licence: GPL v3
+	* Licence URI: https://opensource.org/licenses/GPL-3.0
+	* Text Domain: flickity-slider-elementor
 	*/
 
 	define('ELEMENTOR_FLICKITYSLIDER', __FILE__);
